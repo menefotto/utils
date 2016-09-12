a5a75b1de9881b3dbafc2c5f5c5e6965a962abc2
wind85 <locci.carlo.1985@gmail.com>
2016-09-12 19:31:02 +0200

    added utility functions to crypto lib

--- cryptoutils/cryptoutils.go
+++ cryptoutils/cryptoutils.go
@@ -26,6 +26,16 @@ func FileSha256Sum(filename string) (string, error) {
 
 }
 
+func VerifySha256(sha, filename string) (bool, error) {
+	newsha, err := FileSha256Sum(filename)
+	if err != nil {
+		return false, err
+	}
+
+	return newsha == sha, nil
+
+}
+
 func FileMd5Sum(filename string) (string, error) {
 	f, err := os.Open(filename)
 	if err != nil {
@@ -40,4 +50,13 @@ func FileMd5Sum(filename string) (string, error) {
 
 	return fmt.Sprintf("%x", md5.Sum(content)), nil
 
+}
+
+func VerifyMd5(md5, filename string) (bool, error) {
+	newmd5, err := FileMd5Sum(filename)
+	if err != nil {
+		return false, err
+	}
+
+	return newmd5 == md5, nil
 }
